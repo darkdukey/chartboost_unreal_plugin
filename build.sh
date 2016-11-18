2baf971a37eb2b498cc1d0ac3b1d586d77c11cdb
Nite Luo <darkdukey@gmail.com>
2016-11-17 20:54:52 -0800

    update SDKBOX chartboost plugin

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ UAT_PATH="Build/BatchFiles/"
 
 CURR_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-${UE_PATH}${UAT_PATH}RunUAT.sh BuildPlugin -Plugin="${CURR_PATH}/Plugins/SDKBOX/chartboost/SdkboxChartboost.uplugin" -TargetPlatforms=Mac+IOS+Android -Package="${CURR_PATH}/output"
+${UE_PATH}${UAT_PATH}RunUAT.sh BuildPlugin -Plugin="${CURR_PATH}/Plugins/SDKBOX/chartboost/SdkboxChartboost.uplugin" -TargetPlatforms=Mac+IOS -Package="${CURR_PATH}/output"
 
-cp -R \"${CURR_PATH}/output/Binaries\" \"${CURR_PATH}/Plugins/SDKBOX/chartboost/\"
+cp -R "${CURR_PATH}/output/Binaries" "${CURR_PATH}/Plugins/SDKBOX/chartboost/"
